1f173cef8577986ba0eec9c1c502d234daf87d2e
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-10-24 14:21:16 +0530

    Updates

---
+++ precache-manifest.c086a4ce98019315ce408d7f61b31af4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e0c7576dfab03f889f7fdc31f45ce5",
+    "url": "/reactjs-dscBiet/index.html"
+  },
+  {
+    "revision": "12c6e4d2c85ee2a51aaf",
+    "url": "/reactjs-dscBiet/static/css/2.49933f5d.chunk.css"
+  },
+  {
+    "revision": "120c9f49b3c335fdaba4",
+    "url": "/reactjs-dscBiet/static/css/main.20570a9f.chunk.css"
+  },
+  {
+    "revision": "12c6e4d2c85ee2a51aaf",
+    "url": "/reactjs-dscBiet/static/js/2.9179a8d0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/reactjs-dscBiet/static/js/2.9179a8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120c9f49b3c335fdaba4",
+    "url": "/reactjs-dscBiet/static/js/main.00e0fbe5.chunk.js"
+  },
+  {
+    "revision": "a514198e2a148c8e16e0",
+    "url": "/reactjs-dscBiet/static/js/runtime-main.cd0ef026.js"
+  },
+  {
+    "revision": "6500f189786fcf9150d0ec461fc3edec",
+    "url": "/reactjs-dscBiet/static/media/android.6500f189.svg"
+  },
+  {
+    "revision": "bc4919e0614c757a4f81ea5ecaf87b0a",
+    "url": "/reactjs-dscBiet/static/media/contact134.bc4919e0.webp"
+  },
+  {
+    "revision": "f5f6e3202de1a2fe1495f00abb3a80e9",
+    "url": "/reactjs-dscBiet/static/media/java.f5f6e320.svg"
+  },
+  {
+    "revision": "fa59b833369f41c2586d4892c503508b",
+    "url": "/reactjs-dscBiet/static/media/javascript.fa59b833.svg"
+  },
+  {
+    "revision": "acf672ee4c5c02048bd773763dbb6e15",
+    "url": "/reactjs-dscBiet/static/media/kamal.acf672ee.jpeg"
+  },
+  {
+    "revision": "a1467103f206de3746c2fb7e149e4f63",
+    "url": "/reactjs-dscBiet/static/media/pavan.a1467103.jpeg"
+  },
+  {
+    "revision": "8c97ffb99cd1e312745ae3034bb89ca6",
+    "url": "/reactjs-dscBiet/static/media/python.8c97ffb9.svg"
+  },
+  {
+    "revision": "ee836cc1f9e8dc2c7dbb0d81aa30fa82",
+    "url": "/reactjs-dscBiet/static/media/reactjs.ee836cc1.svg"
+  },
+  {
+    "revision": "65e86a55783c6b789080f590b1fb8cea",
+    "url": "/reactjs-dscBiet/static/media/responsive.65e86a55.svg"
+  },
+  {
+    "revision": "6ed5dded5c5c625785fb4bb97b1e8bd4",
+    "url": "/reactjs-dscBiet/static/media/robot.6ed5dded.svg"
+  },
+  {
+    "revision": "cf580a6f10ceea5a00b45f45aa4e1a9d",
+    "url": "/reactjs-dscBiet/static/media/s1.cf580a6f.webp"
+  },
+  {
+    "revision": "f5ff17ac66955daa907adbd6a1cf3c8b",
+    "url": "/reactjs-dscBiet/static/media/s2.f5ff17ac.webp"
+  },
+  {
+    "revision": "59b310177ade928f3e87674a4c6b8be9",
+    "url": "/reactjs-dscBiet/static/media/s3.59b31017.webp"
+  },
+  {
+    "revision": "27b4b1a71ed7b8755303df4d118c0b1d",
+    "url": "/reactjs-dscBiet/static/media/s4.27b4b1a7.png"
+  }
+]);
